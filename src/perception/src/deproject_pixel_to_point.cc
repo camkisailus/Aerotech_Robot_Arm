c4708bb13374787fba0bc0ca6f366392f28217ed
Cameron Kisailus <camkisailus@gmail.com>
2020-11-25 13:01:23 -0500

    quotes to brackets

--- src/perception/src/deproject_pixel_to_point.cc
+++ src/perception/src/deproject_pixel_to_point.cc
@@ -2,11 +2,11 @@
 This code deprojects pixels into points in 3D space using basic stereo vision techniques. This is integral in the autonomy stack as it takes pixel detections as an input and outputs points in the camera's coordinate frame.
 */
 
-#include "ros/ros.h"
+#include <ros/ros.h>
 
 #include <tf/transform_listener.h>
 #include <sensor_msgs/CameraInfo.h>
-#include "geometry_msgs/PointStamped.h"
+#include <geometry_msgs/PointStamped.h>
 
 #include <unordered_map>
 
